64dc2876b637ea0020e44c46955f6a46066cb0e3
Grimur Jonsson <gimmi@idega.com>
2003-01-21 06:41:12 +0000

    Backed up .1

--- src/java/se/idega/idegaweb/commune/presentation/CommuneSystemStatistics.java
+++ src/java/se/idega/idegaweb/commune/presentation/CommuneSystemStatistics.java
@@ -24,35 +24,19 @@ public class CommuneSystemStatistics extends CommuneBlock {
 	private String HEADER_DEFAULT = "Statistics";
 	private String CITIZEN_ACOUNT_APPLICATIONS_KEY = "citizen_account_applications";
 	private String CITIZEN_ACOUNT_APPLICATIONS_DEFAULT = "Citizen account applications";
-	private String SCHOOL_ALL_CHOICES_KEY = "school_choices_all";
-	private String SCHOOL_ALL_CHOICES_DEFAULT = "Total school choices";
-	private String SCHOOL_HANDLED_CHOICES_KEY = "school_choices_handled";
-	private String SCHOOL_HANDLED_CHOICES_DEFAULT = "Handled school choices_";
-	private String SCHOOL_UNHANDLED_CHOICES_KEY = "school_choices_unhandled";
-	private String SCHOOL_UNHANDLED_CHOICES_DEFAULT = "Unhandled school choices";
+	private String SCHOOL_CHOICES_KEY = "school_choices";
+	private String SCHOOL_CHOICES_DEFAULT = "School choices";
 	
 	private boolean showHeader = true;
 	
-	private int allChoices = 0;
-	private int handledChoices = 0;
-	private int unhandledChoices = 0;
+	private int schoolChoicesCount = 0;
 	private int citizenAccountCount = 0;
 
 	private void init(IWContext iwc) throws Exception {
-		String[] allStatuses = new String[] { SchoolChoiceBMPBean.CASE_STATUS_PRELIMINARY, SchoolChoiceBMPBean.CASE_STATUS_MOVED, SchoolChoiceBMPBean.CASE_STATUS_PLACED };
-		String[] handledStatuses = new String[] { SchoolChoiceBMPBean.CASE_STATUS_PLACED };
-		String[] unhandledStatuses = new String[] { SchoolChoiceBMPBean.CASE_STATUS_PRELIMINARY, SchoolChoiceBMPBean.CASE_STATUS_MOVED };
-		int tempAll = getSchoolChoiceHome().getCount(allStatuses);
-		if (tempAll > 0) {
-			allChoices = tempAll;	
-		}
-		int tempHandled = getSchoolChoiceHome().getCount(handledStatuses);
-		if (tempHandled > 0) {
-			handledChoices = tempHandled;	
-		}
-		int tempUnhandled = getSchoolChoiceHome().getCount(unhandledStatuses);
-		if (tempUnhandled > 0) {
-			unhandledChoices = tempUnhandled;	
+		String[] validStatuses = new String[] { SchoolChoiceBMPBean.CASE_STATUS_PRELIMINARY, SchoolChoiceBMPBean.CASE_STATUS_MOVED };
+		int tempSCC = getSchoolChoiceHome().getCount(validStatuses);
+		if (tempSCC > 0) {
+			schoolChoicesCount = tempSCC;	
 		}
 		int tempCAC = getCitizenAccountHome().getTotalCount();
 		if (tempCAC > 0) {
@@ -71,14 +55,8 @@ public class CommuneSystemStatistics extends CommuneBlock {
 		table.add(getSmallText(getResourceBundle().getLocalizedString(CITIZEN_ACOUNT_APPLICATIONS_KEY, CITIZEN_ACOUNT_APPLICATIONS_DEFAULT)), 1, row);
 		table.add(getSmallText(Integer.toString(citizenAccountCount)), 2, row);
 		++row;
-		table.add(getSmallText(getResourceBundle().getLocalizedString(SCHOOL_ALL_CHOICES_KEY, SCHOOL_ALL_CHOICES_DEFAULT)), 1, row);
-		table.add(getSmallText(Integer.toString(allChoices)), 2, row);
-		++row;
-		table.add(getSmallText(getResourceBundle().getLocalizedString(SCHOOL_HANDLED_CHOICES_KEY, SCHOOL_HANDLED_CHOICES_DEFAULT)), 1, row);
-		table.add(getSmallText(Integer.toString(handledChoices)), 2, row);
-		++row;
-		table.add(getSmallText(getResourceBundle().getLocalizedString(SCHOOL_UNHANDLED_CHOICES_KEY, SCHOOL_UNHANDLED_CHOICES_DEFAULT)), 1, row);
-		table.add(getSmallText(Integer.toString(unhandledChoices)), 2, row);
+		table.add(getSmallText(getResourceBundle().getLocalizedString(SCHOOL_CHOICES_KEY, SCHOOL_CHOICES_DEFAULT)), 1, row);
+		table.add(getSmallText(Integer.toString(schoolChoicesCount)), 2, row);
 		add(table);
 	}
 
