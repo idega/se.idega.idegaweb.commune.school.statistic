be74e0ede41eddf932899bc98fe3862eeec4412e
Thorhallur Helgason <laddi@idega.com>
2004-09-26 10:19:32 +0000

    Added support for music school statistics.

--- src/java/se/idega/idegaweb/commune/presentation/CommuneSystemStatistics.java
+++ src/java/se/idega/idegaweb/commune/presentation/CommuneSystemStatistics.java
@@ -4,6 +4,7 @@ import java.rmi.RemoteException;
 
 import se.idega.idegaweb.commune.account.citizen.business.CitizenAccountBusiness;
 import se.idega.idegaweb.commune.school.business.SchoolChoiceBusiness;
+import se.idega.idegaweb.commune.school.music.business.MusicSchoolBusiness;
 
 import com.idega.business.IBOLookup;
 import com.idega.idegaweb.IWApplicationContext;
@@ -21,14 +22,21 @@ public class CommuneSystemStatistics extends CommuneBlock {
 	private String CITIZEN_ACOUNT_APPLICATIONS_DEFAULT = "Citizen account applications";
 	private String SCHOOL_CHOICES_KEY = "school_choices";
 	private String SCHOOL_CHOICES_DEFAULT = "School choices";
+	private String MUSIC_SCHOOL_CHOICES_TOTAL_KEY = "music_school_choices_total";
+	private String MUSIC_SCHOOL_CHOICES_TOTAL_DEFAULT = "Music school choices";
+	private String MUSIC_SCHOOL_CHOICES_KEY = "music_school_choices";
+	private String MUSIC_SCHOOL_CHOICES_DEFAULT = "Music school choices - first hand";
 	
 	private boolean showHeader = true;
 	
 	private int schoolChoicesCount = 0;
 	private int citizenAccountCount = 0;
+	private int musicChoicesCountTotal = 0;
+	private int musicChoicesCount = 0;
 
 	private boolean showAccountStatistics;
 	private boolean showChoiceStatistics;
+	private boolean showMusicChoiceStatistics;
 	
 	private void init(IWContext iwc) throws Exception {
 		if (isShowChoiceStatistics()) {
@@ -37,6 +45,11 @@ public class CommuneSystemStatistics extends CommuneBlock {
 
 		if (isShowAccountStatistics()) {
 			citizenAccountCount = getCitizenAccountBusiness(iwc).getNumberOfApplications();
+		}
+		
+		if (isShowMusicChoiceStatistics()) {
+			musicChoicesCountTotal = getMusicSchoolBusiness(iwc).getMusicSchoolStatistics(false);
+			musicChoicesCount = getMusicSchoolBusiness(iwc).getMusicSchoolStatistics(true);
 		}
 	}
 
@@ -53,7 +66,13 @@ public class CommuneSystemStatistics extends CommuneBlock {
 		}
 		if (isShowChoiceStatistics()) {
 			table.add(getSmallText(getResourceBundle().getLocalizedString(SCHOOL_CHOICES_KEY, SCHOOL_CHOICES_DEFAULT)), 1, row);
-			table.add(getSmallText(Integer.toString(schoolChoicesCount)), 2, row);
+			table.add(getSmallText(Integer.toString(schoolChoicesCount)), 2, row++);
+		}
+		if (isShowMusicChoiceStatistics()) {
+			table.add(getSmallText(getResourceBundle().getLocalizedString(MUSIC_SCHOOL_CHOICES_TOTAL_KEY, MUSIC_SCHOOL_CHOICES_TOTAL_DEFAULT)), 1, row);
+			table.add(getSmallText(Integer.toString(musicChoicesCountTotal)), 2, row++);
+			table.add(getSmallText(getResourceBundle().getLocalizedString(MUSIC_SCHOOL_CHOICES_KEY, MUSIC_SCHOOL_CHOICES_DEFAULT)), 1, row);
+			table.add(getSmallText(Integer.toString(musicChoicesCount)), 2, row++);
 		}
 		add(table);
 	}
@@ -73,6 +92,17 @@ public class CommuneSystemStatistics extends CommuneBlock {
 		return (CitizenAccountBusiness) IBOLookup.getServiceInstance(iwac, CitizenAccountBusiness.class);	
 	}
 	
+	protected MusicSchoolBusiness getMusicSchoolBusiness(IWApplicationContext iwac) throws RemoteException {
+		return (MusicSchoolBusiness) IBOLookup.getServiceInstance(iwac, MusicSchoolBusiness.class);	
+	}
+	
+	/**
+	 * @return boolean
+	 */
+	public boolean isShowMusicChoiceStatistics() {
+		return showMusicChoiceStatistics;
+	}
+
 	/**
 	 * @return boolean
 	 */
@@ -92,6 +122,14 @@ public class CommuneSystemStatistics extends CommuneBlock {
 	 */
 	public boolean isShowHeader() {
 		return showHeader;
+	}
+
+	/**
+	 * Sets the showChoiceStatistics.
+	 * @param showChoiceStatistics The showChoiceStatistics to set
+	 */
+	public void setShowMusicChoiceStatistics(boolean showMusicChoiceStatistics) {
+		this.showMusicChoiceStatistics = showMusicChoiceStatistics;
 	}
 
 	/**
