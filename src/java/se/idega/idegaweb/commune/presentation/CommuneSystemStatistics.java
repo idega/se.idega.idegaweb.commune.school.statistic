b5f5f3c341bbb58abe191948198651bcce4dadf6
Malin Anulf <malin.anulf@agurait.com>
2004-12-14 15:26:23 +0000

    added the possibility to display the total number of vaccancies in child care

--- src/java/se/idega/idegaweb/commune/presentation/CommuneSystemStatistics.java
+++ src/java/se/idega/idegaweb/commune/presentation/CommuneSystemStatistics.java
@@ -1,12 +1,20 @@
 package se.idega.idegaweb.commune.presentation;
 
 import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
 
 import se.idega.idegaweb.commune.account.citizen.business.CitizenAccountBusiness;
+import se.idega.idegaweb.commune.childcare.business.ChildCareBusiness;
+import se.idega.idegaweb.commune.childcare.data.ChildCarePrognosis;
 import se.idega.idegaweb.commune.school.business.SchoolChoiceBusiness;
 import se.idega.idegaweb.commune.school.music.business.MusicSchoolBusiness;
 
+import com.idega.block.school.data.School;
 import com.idega.business.IBOLookup;
+import com.idega.core.location.business.CommuneBusiness;
 import com.idega.idegaweb.IWApplicationContext;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Table;
@@ -27,6 +35,9 @@ public class CommuneSystemStatistics extends CommuneBlock {
 	private String MUSIC_SCHOOL_CHOICES_TOTAL_DEFAULT = "Music school choices";
 	private String MUSIC_SCHOOL_CHOICES_KEY = "music_school_choices";
 	private String MUSIC_SCHOOL_CHOICES_DEFAULT = "Music school choices - first hand";
+	private String CHILDCARE_VACANCIES_KEY = "childcare_vacancies";
+	private String CHILDCARE_VACANCIES_DEFAULT = "Childcare vacancies";
+	
 	
 	private boolean showHeader = true;
 	private String styleClass=null;
@@ -35,10 +46,14 @@ public class CommuneSystemStatistics extends CommuneBlock {
 	private int citizenAccountCount = 0;
 	private int musicChoicesCountTotal = 0;
 	private int musicChoicesCount = 0;
-
+	private int providerVacanciesCount = 0;
+	
+	private ChildCareBusiness business;
+	
 	private boolean showAccountStatistics;
 	private boolean showChoiceStatistics;
 	private boolean showMusicChoiceStatistics;
+	private boolean showChildCareVacanices;
 	
 	private void init(IWContext iwc) throws Exception {
 		if (isShowChoiceStatistics()) {
@@ -53,51 +68,42 @@ public class CommuneSystemStatistics extends CommuneBlock {
 			musicChoicesCountTotal = getMusicSchoolBusiness(iwc).getMusicSchoolStatistics(false);
 			musicChoicesCount = getMusicSchoolBusiness(iwc).getMusicSchoolStatistics(true);
 		}
+		
+		if (showChildCareVacanices){
+			providerVacanciesCount = getProviderVacancies(iwc);	
+		}
+						
+		
 	}
 
 	private void drawTable() {
 		Table table = new Table();
 		int row = 1;
+		
 		if (isShowHeader()) {
 			table.mergeCells(1, row, 2, row);
 			table.add(getSmallHeader(getResourceBundle().getLocalizedString(HEADER_KEY, HEADER_DEFAULT)), 1, row++);
 		}
 		if (isShowAccountStatistics()) {
-			if (styleClass != null){
-				table.add(getStyledClass(getResourceBundle().getLocalizedString(CITIZEN_ACOUNT_APPLICATIONS_KEY, CITIZEN_ACOUNT_APPLICATIONS_DEFAULT)), 1, row);
-				table.add(getStyledClass(Integer.toString(citizenAccountCount)), 2, row++);
-				
-			}
-			else {
-				table.add(getSmallText(getResourceBundle().getLocalizedString(CITIZEN_ACOUNT_APPLICATIONS_KEY, CITIZEN_ACOUNT_APPLICATIONS_DEFAULT)), 1, row);
-				table.add(getSmallText(Integer.toString(citizenAccountCount)), 2, row++);	
-			}
-			
+			table.add(getWhichStyle(getResourceBundle().getLocalizedString(CITIZEN_ACOUNT_APPLICATIONS_KEY, CITIZEN_ACOUNT_APPLICATIONS_DEFAULT)), 1, row);
+			table.add(getWhichStyle(Integer.toString(citizenAccountCount)), 2, row++);
 		}
-		if (isShowChoiceStatistics()) {
-			if (styleClass != null){
-				table.add(getStyledClass(getResourceBundle().getLocalizedString(SCHOOL_CHOICES_KEY, SCHOOL_CHOICES_DEFAULT)), 1, row);
-				table.add(getStyledClass(Integer.toString(schoolChoicesCount)), 2, row++);	
-			}
-			else {
-				table.add(getSmallText(getResourceBundle().getLocalizedString(SCHOOL_CHOICES_KEY, SCHOOL_CHOICES_DEFAULT)), 1, row);
-				table.add(getSmallText(Integer.toString(schoolChoicesCount)), 2, row++);	
-			}
-			
+		if (isShowChoiceStatistics()){
+			table.add(getWhichStyle(getResourceBundle().getLocalizedString(SCHOOL_CHOICES_KEY, SCHOOL_CHOICES_DEFAULT)), 1, row);
+			table.add(getWhichStyle(Integer.toString(schoolChoicesCount)), 2, row++);	
 		}
+		
+		if (showChildCareVacanices){
+			table.add(getWhichStyle(getResourceBundle().getLocalizedString(CHILDCARE_VACANCIES_KEY, CHILDCARE_VACANCIES_DEFAULT)), 1, row);
+			table.add(getWhichStyle(Integer.toString(providerVacanciesCount)), 2, row++);	
+		}
+		
 		if (isShowMusicChoiceStatistics()) {
-			if (styleClass != null){
-				table.add(getStyledClass(getResourceBundle().getLocalizedString(MUSIC_SCHOOL_CHOICES_TOTAL_KEY, MUSIC_SCHOOL_CHOICES_TOTAL_DEFAULT)), 1, row);
-				table.add(getStyledClass(Integer.toString(musicChoicesCountTotal)), 2, row++);
-				table.add(getStyledClass(getResourceBundle().getLocalizedString(MUSIC_SCHOOL_CHOICES_KEY, MUSIC_SCHOOL_CHOICES_DEFAULT)), 1, row);
-				table.add(getStyledClass(Integer.toString(musicChoicesCount)), 2, row++);
-			}
-			else{
-				table.add(getSmallText(getResourceBundle().getLocalizedString(MUSIC_SCHOOL_CHOICES_TOTAL_KEY, MUSIC_SCHOOL_CHOICES_TOTAL_DEFAULT)), 1, row);
-				table.add(getSmallText(Integer.toString(musicChoicesCountTotal)), 2, row++);
-				table.add(getSmallText(getResourceBundle().getLocalizedString(MUSIC_SCHOOL_CHOICES_KEY, MUSIC_SCHOOL_CHOICES_DEFAULT)), 1, row);
-				table.add(getSmallText(Integer.toString(musicChoicesCount)), 2, row++);
-			}
+			table.add(getWhichStyle(getResourceBundle().getLocalizedString(MUSIC_SCHOOL_CHOICES_TOTAL_KEY, MUSIC_SCHOOL_CHOICES_TOTAL_DEFAULT)), 1, row);
+			table.add(getWhichStyle(Integer.toString(musicChoicesCountTotal)), 2, row++);
+			table.add(getWhichStyle(getResourceBundle().getLocalizedString(MUSIC_SCHOOL_CHOICES_KEY, MUSIC_SCHOOL_CHOICES_DEFAULT)), 1, row);
+			table.add(getWhichStyle(Integer.toString(musicChoicesCount)), 2, row++);
+			
 			
 		}
 		add(table);
@@ -108,6 +114,42 @@ public class CommuneSystemStatistics extends CommuneBlock {
 		super.main(iwc);
 		init(iwc);
 		drawTable();	
+	}
+	
+	private int getProviderVacancies(IWContext iwc) throws Exception {
+		Collection schoolTypes = null;
+		schoolTypes = getBusiness(iwc).getSchoolBusiness().findAllSchoolTypesForChildCare();
+		
+		List providers = null;
+		int providerVacanciesSum = 0;
+		
+		providers = new Vector(getBusiness(iwc).getSchoolBusiness().findAllSchoolsByType(schoolTypes));
+		if (providers != null && !providers.isEmpty()) {
+			int defaultCommuneId = ((Integer) getCommuneBusiness(iwc).getDefaultCommune().getPrimaryKey()).intValue();
+			School school;
+			ChildCarePrognosis prognosis;
+			int providerID = -1;
+			
+			Iterator iter = providers.iterator();
+			while (iter.hasNext()) {
+				school = (School) iter.next();
+				if (school.getCommuneId() != defaultCommuneId) {
+					continue;
+				}
+				providerID = ((Integer)school.getPrimaryKey()).intValue();
+				prognosis = getBusiness(iwc).getPrognosis(providerID);
+				
+				
+				if (prognosis != null) {
+					int providerVacancies = prognosis.getVacancies();
+					if (providerVacancies!= -1)
+						providerVacanciesSum += providerVacancies;
+				}
+				
+			}
+		}
+		
+		return providerVacanciesSum;
 	}
 	
 	protected SchoolChoiceBusiness getSchoolChoiceBusiness(IWApplicationContext iwac) throws RemoteException {
@@ -122,9 +164,20 @@ public class CommuneSystemStatistics extends CommuneBlock {
 		return (MusicSchoolBusiness) IBOLookup.getServiceInstance(iwac, MusicSchoolBusiness.class);	
 	}
 	
+		
+	public Text getWhichStyle(String s){
+		if (styleClass != null){
+			return getStyledClass(s);
+		}
+		else{
+			return getSmallText(s);
+		}
+	}
+	
 	public Text getStyledClass(String s) {
 		return getStyleText(s, styleClass);
 	}
+	
 	
 	/**
 	 * @return boolean
@@ -154,6 +207,14 @@ public class CommuneSystemStatistics extends CommuneBlock {
 		return showHeader;
 	}
 
+	/**
+	 * Sets the showChildCareVacanices.
+	 * @param showChoiceStatistics The showChoiceStatistics to set
+	 */
+	public void setShowChildCareVacanices(boolean showChildCareVacanices) {
+		this.showChildCareVacanices = showChildCareVacanices;
+	}
+	
 	/**
 	 * Sets the showChoiceStatistics.
 	 * @param showChoiceStatistics The showChoiceStatistics to set
@@ -192,5 +253,13 @@ public class CommuneSystemStatistics extends CommuneBlock {
 	public void setStyleClass(String styleClass)
 	{
 		this.styleClass = styleClass;
+	}
+	
+	protected CommuneBusiness getCommuneBusiness(IWApplicationContext iwac) throws RemoteException {
+		return (CommuneBusiness) IBOLookup.getServiceInstance(iwac, CommuneBusiness.class);
+	}
+	
+	protected ChildCareBusiness getBusiness(IWApplicationContext iwac) throws RemoteException {
+		return (ChildCareBusiness) IBOLookup.getServiceInstance(iwac, ChildCareBusiness.class);
 	}
 }
